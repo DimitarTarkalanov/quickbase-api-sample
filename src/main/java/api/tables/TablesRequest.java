db264e007f358483f919e36e32f4fad80ef96fa6
Dimitar Tarkalanov <dimitar.tarkalanov@gmail.com>
2022-06-26 00:51:51 +0300

    create TableRequest

---
+++ src/main/java/api/tables/TablesRequest.java
@@ -0,0 +1,24 @@
+package api.tables;
+
+import api.Request;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.Map;
+
+public class TablesRequest extends Request {
+    private static final String TABLES_PATH = "/tables";
+    private final String appId;
+
+    public TablesRequest(String baseUri, int statusCode, String appId) {
+        super(baseUri, TABLES_PATH, statusCode, RequestMethod.GET);
+        this.appId = appId;
+    }
+
+    protected RequestSpecification initGetRequest() {
+        return super.initRequest(
+                Map.of(
+                        "appId", appId
+                )
+        );
+    }
+}
