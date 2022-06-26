bc0912eef71646794b755f311e7010440f8a2619
Dimitar Tarkalanov <dimitar.tarkalanov@gmail.com>
2022-06-26 19:57:30 +0300

    Create FieldsRequest

---
+++ src/main/java/api/fields/FieldsRequest.java
@@ -0,0 +1,24 @@
+package api.fields;
+
+import api.Request;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.Map;
+
+public class FieldsRequest extends Request {
+    private static final String TABLES_PATH = "/fields";
+    private final String tableId;
+
+    public FieldsRequest(String baseUri, int statusCode, String tableId) {
+        super(baseUri, TABLES_PATH, statusCode, RequestMethod.GET);
+        this.tableId = tableId;
+    }
+
+    protected RequestSpecification initGetRequest() {
+        return super.initRequest(
+                Map.of(
+                        "tableId", tableId
+                )
+        );
+    }
+}
