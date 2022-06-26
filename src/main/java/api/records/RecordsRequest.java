7f93da8ab12cfd83b92f40a7a90dfdcfff84239b
Dimitar Tarkalanov <dimitar.tarkalanov@gmail.com>
2022-06-26 23:16:41 +0300

    add constructor - RecordsRequest

--- src/main/java/api/records/RecordsRequest.java
+++ src/main/java/api/records/RecordsRequest.java
@@ -5,9 +5,11 @@ import api.Request;
 public class RecordsRequest extends Request {
     private static final String RECORDS_PATH = "/records";
 
-    public RecordsRequest(String baseUri, int statusCode, RequestMethod requestMethod, RecordsPayload payload) {
-        super(baseUri, RECORDS_PATH, statusCode, requestMethod, payload);
+    public RecordsRequest(String baseUri, int statusCode, RecordsPayload payload) {
+        super(baseUri, RECORDS_PATH, statusCode, RequestMethod.POST, payload);
     }
 
-
+    public RecordsRequest(String baseUri, int statusCode, RecordsQueryPayload payload) {
+        super(baseUri, RECORDS_PATH, statusCode, RequestMethod.DELETE, payload);
+    }
 }
